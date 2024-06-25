2d15f5e950f3cbb8d451ca8e7cacb2a7501fb347
MihirSaiDudekula <mihirsaidudekula@gmail.com>
2024-06-26 00:25:18 +0530

    explained jwt usage in this context

--- week-3/04-mongo-with-jwt-auth/solution/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/solution/middleware/admin.js
@@ -8,6 +8,18 @@ function adminMiddleware(req, res, next) {
     const jwtToken = words[1]; // token
     try {
         const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+         // It checks that the JWT token's signature is valid using the secret key (JWT_SECRET). This ensures that the token was not tampered with after it was signed.
+        // If the signature is valid, it decodes the payload of the token. The payload contains the claims (information) that were encoded into the token when it was created.
+        //decoded example
+        /*
+        {
+          "userId": "12345",
+          "username": "johndoe",
+          "iat": 1620000000,
+          "exp": 1620003600
+        }
+
+        */
         if (decodedValue.username) {
             next();
         } else {
