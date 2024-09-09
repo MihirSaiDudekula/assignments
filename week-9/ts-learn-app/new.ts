c0941d15e10d17607dbca85fdd873f4ab674f542
MihirSaiDudekula <mihirsaidudekula@gmail.com>
2024-09-09 22:13:46 +0530

    ts continued

--- week-9/ts-learn-app/new.ts
+++ week-9/ts-learn-app/new.ts
@@ -1,45 +1,51 @@
-const x: number = 1;
-console.log(x);
-
-// code that gives errors
-// let x: number = 1;
-// x = "harkirat";
+// const x: number = 1;
 // console.log(x);
 
-function greet(firstName: string): void {
-    // Implementation goes here
-    console.log(`Hello ${firstName}`)
-}
+// // code that gives errors
+// // let x: number = 1;
+// // x = "harkirat";
+// // console.log(x);
 
-greet("Mihir")
+// function greet(firstName: string): void {
+//     // Implementation goes here
+//     console.log(`Hello ${firstName}`)
+// }
 
-function sum(a: number, b: number): number {
-    // Implementation goes here
-    return a+b;
-}
+// greet("Mihir")
 
-console.log(sum(5,6));
+// function sum(a: number, b: number): number {
+//     // Implementation goes here
+//     return a+b;
+// }
 
-function isLegal(age: number): boolean {
-    // Implementation goes here
-    if(age>=18)
-    {
-    	return true
-    }
-    else
-    {
-    	return false
-    }
-}
+// console.log(sum(5,6));
 
-console.log(isLegal(21))
+// function isLegal(age: number): boolean {
+//     // Implementation goes here
+//     if(age>=18)
+//     {
+//     	return true
+//     }
+//     else
+//     {
+//     	return false
+//     }
+// }
 
-function delayed(fn:()=>void):void{
-	setTimeout(fn,1000);
-}
+// console.log(isLegal(21))
 
-function torun():void{
-	console.log("hi")
-}
+// function delayed(fn:()=>void):void{
+// 	setTimeout(fn,1000);
+// }
 
-delayed(torun)
+// function torun():void{
+// 	console.log("hi")
+// }
+
+// delayed(torun)
+// Define an interface to specify the structure of a todo object
+
+let name: string | null = null;
+
+// we use the non-null assertion operator to tell the compiler that name will never be null
+let nameLength = name!.length;
